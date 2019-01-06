85b4c9ecfe126740c52cb7ea3a2340b66956f66f
Ratson <github@ratson.name>
2019-01-06 23:24:34 +0800

    Add react-apollo

--- pages/about.js
+++ pages/about.js
@@ -1,21 +1,21 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
 
-import React from 'react';
-import PropTypes from 'prop-types';
-import Button from '@material-ui/core/Button';
-import Typography from '@material-ui/core/Typography';
-import { withStyles } from '@material-ui/core/styles';
-import Link from 'next/link';
+import React from 'react'
+import PropTypes from 'prop-types'
+import Button from '@material-ui/core/Button'
+import Typography from '@material-ui/core/Typography'
+import { withStyles } from '@material-ui/core/styles'
+import Link from 'next/link'
 
 const styles = theme => ({
   root: {
     textAlign: 'center',
     paddingTop: theme.spacing.unit * 20,
   },
-});
+})
 
 function About(props) {
-  const { classes } = props;
+  const { classes } = props
 
   return (
     <div className={classes.root}>
@@ -34,11 +34,11 @@ function About(props) {
         Do nothing button
       </Button>
     </div>
-  );
+  )
 }
 
 About.propTypes = {
   classes: PropTypes.object.isRequired,
-};
+}
 
-export default withStyles(styles)(About);
+export default withStyles(styles)(About)
--- pages/index.js
+++ pages/index.js
@@ -1,44 +1,46 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
 
-import React from 'react';
-import PropTypes from 'prop-types';
-import Button from '@material-ui/core/Button';
-import Dialog from '@material-ui/core/Dialog';
-import DialogTitle from '@material-ui/core/DialogTitle';
-import DialogContent from '@material-ui/core/DialogContent';
-import DialogContentText from '@material-ui/core/DialogContentText';
-import DialogActions from '@material-ui/core/DialogActions';
-import Typography from '@material-ui/core/Typography';
-import { withStyles } from '@material-ui/core/styles';
-import Link from 'next/link';
+import React from 'react'
+import PropTypes from 'prop-types'
+import Button from '@material-ui/core/Button'
+import Dialog from '@material-ui/core/Dialog'
+import DialogTitle from '@material-ui/core/DialogTitle'
+import DialogContent from '@material-ui/core/DialogContent'
+import DialogContentText from '@material-ui/core/DialogContentText'
+import DialogActions from '@material-ui/core/DialogActions'
+import Typography from '@material-ui/core/Typography'
+import { withStyles } from '@material-ui/core/styles'
+import Link from 'next/link'
+import { graphql } from 'react-apollo'
+import gql from 'graphql-tag'
 
 const styles = theme => ({
   root: {
     textAlign: 'center',
     paddingTop: theme.spacing.unit * 20,
   },
-});
+})
 
 class Index extends React.Component {
   state = {
     open: false,
-  };
+  }
 
   handleClose = () => {
     this.setState({
       open: false,
-    });
-  };
+    })
+  }
 
   handleClick = () => {
     this.setState({
       open: true,
-    });
-  };
+    })
+  }
 
   render() {
-    const { classes } = this.props;
-    const { open } = this.state;
+    const { classes, data } = this.props
+    const { open } = this.state
 
     return (
       <div className={classes.root}>
@@ -64,16 +66,25 @@ class Index extends React.Component {
             <a>Go to the about page</a>
           </Link>
         </Typography>
-        <Button variant="contained" color="secondary" onClick={this.handleClick}>
-          Super Secret Password
+        <Button
+          variant="contained"
+          color="secondary"
+          onClick={this.handleClick}>
+          {data.greeting ? data.greeting.message : 'Loading...'}
         </Button>
       </div>
-    );
+    )
   }
 }
 
 Index.propTypes = {
   classes: PropTypes.object.isRequired,
-};
+}
 
-export default withStyles(styles)(Index);
+export default graphql(gql`
+  query GreetingQuery {
+    greeting {
+      message
+    }
+  }
+`)(withStyles(styles)(Index))
