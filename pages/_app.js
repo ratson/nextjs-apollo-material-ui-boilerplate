f350ff2b05e135cd7e4dc735adb59cc35d6564c1
Ratson <github@ratson.name>
2019-01-08 21:53:49 +0800

    Add react-intl

--- pages/_app.js
+++ pages/_app.js
@@ -5,6 +5,7 @@ import { MuiThemeProvider } from '@material-ui/core/styles'
 import CssBaseline from '@material-ui/core/CssBaseline'
 import JssProvider from 'react-jss/lib/JssProvider'
 import { ApolloProvider } from 'react-apollo'
+import { IntlProvider } from 'react-intl'
 
 import getPageContext from 'app/src/getPageContext'
 import withApolloClient from 'app/lib/with-apollo-client'
@@ -27,27 +28,29 @@ class MyApp extends App {
     const { Component, pageProps, apolloClient } = this.props
     return (
       <Container>
-        <ApolloProvider client={apolloClient}>
-          <Head>
-            <title>My page</title>
-          </Head>
-          {/* Wrap every page in Jss and Theme providers */}
-          <JssProvider
-            registry={this.pageContext.sheetsRegistry}
-            generateClassName={this.pageContext.generateClassName}>
-            {/* MuiThemeProvider makes the theme available down the React
+        <IntlProvider locale="en">
+          <ApolloProvider client={apolloClient}>
+            <Head>
+              <title>My page</title>
+            </Head>
+            {/* Wrap every page in Jss and Theme providers */}
+            <JssProvider
+              registry={this.pageContext.sheetsRegistry}
+              generateClassName={this.pageContext.generateClassName}>
+              {/* MuiThemeProvider makes the theme available down the React
               tree thanks to React context. */}
-            <MuiThemeProvider
-              theme={this.pageContext.theme}
-              sheetsManager={this.pageContext.sheetsManager}>
-              {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
-              <CssBaseline />
-              {/* Pass pageContext to the _document though the renderPage enhancer
+              <MuiThemeProvider
+                theme={this.pageContext.theme}
+                sheetsManager={this.pageContext.sheetsManager}>
+                {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+                <CssBaseline />
+                {/* Pass pageContext to the _document though the renderPage enhancer
                 to render collected styles on server-side. */}
-              <Component pageContext={this.pageContext} {...pageProps} />
-            </MuiThemeProvider>
-          </JssProvider>
-        </ApolloProvider>
+                <Component pageContext={this.pageContext} {...pageProps} />
+              </MuiThemeProvider>
+            </JssProvider>
+          </ApolloProvider>
+        </IntlProvider>
       </Container>
     )
   }
--- pages/counter.js
+++ pages/counter.js
@@ -3,6 +3,7 @@ import gql from 'graphql-tag'
 import { Mutation, Query } from 'react-apollo'
 import Button from '@material-ui/core/Button'
 import Link from 'next/link'
+import { FormattedMessage } from 'react-intl'
 
 const GET_COUNTER = gql`
   {
@@ -21,13 +22,20 @@ const INCREMENT_COUNTER = gql`
 `
 
 const IncrementButton = ({ children }) => (
-  <Mutation mutation={INCREMENT_COUNTER} variables={{ step: 1 }}>
+  <Mutation
+    mutation={INCREMENT_COUNTER}
+    variables={{ step: children === 0 ? 1 : children + 1 }}
+    refetchQueries={() => [
+      {
+        query: GET_COUNTER,
+      },
+    ]}>
     {(increment, { data }) => (
       <Button
         variant="contained"
         color="primary"
         onClick={() => {
-          increment(1)
+          increment()
         }}>
         {data ? data.increment.count : children}
       </Button>
@@ -40,10 +48,20 @@ const Counter = () => (
     {({ loading, error, data }) => {
       if (loading) return 'Loading...'
       if (error) return `Error! ${error.message}`
-
+      const name = 'World'
       return (
         <>
           <IncrementButton>{data.counter.count}</IncrementButton>
+          <p>
+            <FormattedMessage
+              id="welcome"
+              defaultMessage={`Hello {name}, you have {unreadCount, number} {unreadCount, plural, one {message} other {messages}}`}
+              values={{
+                name: <b>{name}</b>,
+                unreadCount: data.counter.count,
+              }}
+            />
+          </p>
           <p>
             <Link href="/">Home</Link>
           </p>
