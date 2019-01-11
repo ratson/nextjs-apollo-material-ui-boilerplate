4259911ca50c3a1c7a5ba9a977010e947f9efcbf
Ratson <github@ratson.name>
2019-01-11 20:37:23 +0800

    Utilize fs-extra to write JSON with EOF newline

--- scripts/default-lang.js
+++ scripts/default-lang.js
@@ -1,20 +1,23 @@
-const { readFileSync, writeFileSync } = require('fs')
 const { resolve } = require('path')
+const fse = require('fs-extra')
 const glob = require('glob')
 
-const defaultMessages = glob
-  .sync('./lang/.messages/**/*.json')
-  .map(filename => readFileSync(filename, 'utf8'))
-  .map(file => JSON.parse(file))
-  .reduce((messages, descriptors) => {
-    descriptors.forEach(({ id, defaultMessage }) => {
-      if (messages.hasOwnProperty(id)) {
-        throw new Error(`Duplicate message id: ${id}`)
-      }
-      messages[id] = defaultMessage
-    })
-    return messages
-  }, {})
+async function main() {
+  const defaultMessages = glob
+    .sync('./lang/.messages/**/*.json')
+    .map(filename => fse.readJSONSync(filename))
+    .reduce((messages, descriptors) => {
+      descriptors.forEach(({ id, defaultMessage }) => {
+        if (messages.hasOwnProperty(id)) {
+          throw new Error(`Duplicate message id: ${id}`)
+        }
+        messages[id] = defaultMessage
+      })
+      return messages
+    }, {})
 
-writeFileSync('./lang/en.json', JSON.stringify(defaultMessages, null, 2))
-console.log(`> Wrote default messages to: "${resolve('./lang/en.json')}"`)
+  await fse.outputJSON('./lang/en.json', defaultMessages, { spaces: 2 })
+  console.log(`> Wrote default messages to: "${resolve('./lang/en.json')}"`)
+}
+
+main()
